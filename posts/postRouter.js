ec2996770434cb814a5ec7899f5285856e165c61
Oliver-Strange <preston.burton@gmail.com>
2019-06-05 13:22:40 -0700

    started working on postRouter and realised it is stretch, now working on userRouter

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,36 @@
-const express = 'express';
+const express = require("express");
+
+const Posts = require("./postDb");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {});
 
-});
+router.get("/:id", (req, res) => {});
 
-router.get('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {});
 
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put("/:id", (req, res) => {});
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-
-};
+  const { id } = req.params;
+  Posts.getById(id)
+    .then(post => {
+      if (post) {
+        req.post = post;
+        next();
+      } else {
+        res
+          .status(404)
+          .json({ message: "Post Id is invalid; Post Id was not found" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Server failed to process request." });
+    });
+}
 
 module.exports = router;
