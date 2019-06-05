f376c6b0e196a4d14bf6efb1253d3fe7b1ef95a9
Oliver-Strange <preston.burton@gmail.com>
2019-06-05 13:30:51 -0700

    set up validateUserId

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const User = require("./userDb");
+const Users = require("./userDb");
 
 const router = express.Router();
 
@@ -20,7 +20,22 @@ router.put("/:id", (req, res) => {});
 
 //custom middleware
 
-function validateUserId(req, res, next) {}
+function validateUserId(req, res, next) {
+  const { id } = req.params;
+  Users.getById(id)
+    .then(user => {
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        res.status(400).json({ message: "invalid user id" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "failed to process request" });
+    });
+}
 
 function validateUser(req, res, next) {}
 
