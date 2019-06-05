026749384f55bae000937bb21fdd147a53aba833
Oliver-Strange <preston.burton@gmail.com>
2019-06-05 13:06:07 -0700

    set up server.js with express, morgan, helmet, and logger method. Server.used them and added route handlers for posts and user

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-const express = 'express';
+const express = require("express");
+const helmet = require("helmet");
+const logger = require("morgan");
+
+const postsRouter = require("./posts/postRouter");
+const userRouter = require("./users/userRouter");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+// Global Middleware
+server.use(express.json());
+server.use(helmet());
+server.use(logger("dev"));
+server.use(methodLogger);
+
+// Route Handlers
+server.use("/api/posts", postsRouter);
+server.use("/api/user", userRouter);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {
-
-};
+function methodLogger(req, res, next) {
+  console.log(`${req.method} Request. ${Date.getTime()}`);
+  next();
+}
 
 module.exports = server;
